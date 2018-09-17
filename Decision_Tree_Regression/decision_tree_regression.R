c3efbb0fdfbd295b15961426471604be5125e712
SonaliDasgupta <sonalidasgupta95.2011@gmail.com>
2018-09-17 21:08:47 +0530

    Decision Tree Regression in R

--- Decision_Tree_Regression/decision_tree_regression.R
+++ Decision_Tree_Regression/decision_tree_regression.R
@@ -1,44 +1,26 @@
-# Decision Tree Regression
+dataset = read.csv("Position_Salaries.csv")
 
-# Importing the dataset
-dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
+#Feature Scaling
 
-# Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
-# library(caTools)
-# set.seed(123)
-# split = sample.split(dataset$Salary, SplitRatio = 2/3)
-# training_set = subset(dataset, split == TRUE)
-# test_set = subset(dataset, split == FALSE)
-
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
-
-# Fitting Decision Tree Regression to the dataset
-# install.packages('rpart')
+# regressor
+#create regressor
 library(rpart)
-regressor = rpart(formula = Salary ~ .,
-                  data = dataset,
-                  control = rpart.control(minsplit = 1))
+regressor= rpart(formula= Salary ~ ., data= dataset, control = rpart.control(minsplit=1))
 
-# Predicting a new result with Decision Tree Regression
+#predict with regression
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-# Visualising the Decision Tree Regression results (higher resolution)
-# install.packages('ggplot2')
-library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Decision Tree Regression)') +
-  xlab('Level') +
-  ylab('Salary')
 
-# Plotting the tree
-plot(regressor)
-text(regressor)
+#visualize regression
+library(ggplot2)
+
+
+
+#visualize regression smooth curve
+
+x_grid= seq(min(dataset$Level), max(dataset$Level),0.01)
+ggplot() + geom_point(aes(x= dataset$Level, y=dataset$Salary), color='red')+ geom_line(aes(x=x_grid, y=predict(regressor, newdata= data.frame(Level= x_grid))), color='blue')
++ ggtitle("Decision Tree")+ xlab("Level")+ ylab("Salary")
+
+
